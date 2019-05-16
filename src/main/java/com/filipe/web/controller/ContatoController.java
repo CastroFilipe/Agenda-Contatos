891f874b48338456d3e62651b3905b88a3d4da3a
Filipe Castro <filipe.couto.10@gmail.com>
2019-05-16 12:15:30 -0300

    modificações nos templates

---
+++ src/main/java/com/filipe/web/controller/ContatoController.java
@@ -0,0 +1,20 @@
+package com.filipe.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/contatos")
+public class ContatoController {
+
+	@GetMapping("/cadastrar")
+	public String cadastrar() {
+		return "/contato/cadastro.html";
+	}
+	
+	@GetMapping("/listar")
+	public String listar() {
+		return "/contato/lista.html";
+	}
+}
